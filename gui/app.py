f8ffb3e2528035d8091fd36531d6def2762c29e9
Ahmed-Khaled-L <ahmed.khaled.mohamed.211@gmail.com>
2026-05-09 06:46:55 +0300

    Update map file and change optimizer to GreedyOptimizer for improved routing

--- gui/app.py
+++ gui/app.py
@@ -12,7 +12,7 @@ from utils.metrics_logger import MetricsLogger
 class App:
     def __init__(self):
         # 1. Define and Load the Map
-        map_filepath = "boards/Level3_4_deadlock.json"  # <-- NEW: Data-Driven JSON Map Loader
+        map_filepath = "boards/level99_titan_mcu.json"  # <-- NEW: Data-Driven JSON Map Loader
         
         # Extract just the benchmark name (e.g., "test2_mega_mcu")
         self.benchmark_name = map_filepath.split('/')[-1].replace('.json', '')
@@ -21,7 +21,7 @@ class App:
         
         # 2. Setup the Strategy Pattern (Router + Optimizer)
         self.router = AStarRouter(self.grid)
-        self.optimizer = SimulatedAnnealingOptimizer(self.grid, self.router)
+        self.optimizer = GreedyOptimizer(self.grid, self.router)
         
         # Inject the optimizer into the manager
         self.manager.optimizer = self.optimizer
